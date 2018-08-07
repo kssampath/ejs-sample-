1e481eb9f94844f2843a5082ba642b3ead74ab28
sai sampath k <sskolla0797@gmail.com>
2018-08-07 18:15:19 +0530

    Update ejs.js

--- ejs.js
+++ ejs.js
@@ -2,12 +2,12 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var cors = require('cors');
 var path = require('path');
-var expressLayouts = require('express-ejs-layouts');
+
 
 var app = express();
 app.use(bodyParser());
 app.use(cors());
-app.use(expressLayouts());
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.listen(8080,function(){
@@ -22,11 +22,13 @@ app.get('/hero', function (req, res) {
 		head : 'THE MOVIE WORLD',
 		neck : 'THIS IS THE PLACE FOR HOT ACTRESS',
 		actress : [
-			{ name: 'RIYA RANJAN', age: '16' },
-			{ name: 'SAGRIKA CHAUHAN' ,age: '18' },
-			{ name: 'NEHA MAHESWARI' ,age: '20' },
-			{ name: 'KRITIKA KOUSHIK', age: '1infinity' }// I am 20 years old -Omkar
+			{ name: 'RIYA RANJAN', age: '16' img: 'riya' },
+			{ name: 'SAGRIKA CHAUHAN' ,age: '18' img: 'sag' },
+			{ name: 'NEHA MAHESWARI' ,age: '20' img: 'neha'},
+			{ name: 'KRITIKA KOUSHIK', age: '1infinity' img: 'krit' }
 		]
-		//with injecting variables
+	
+					
+					//with injecting variables
 	});
 });
